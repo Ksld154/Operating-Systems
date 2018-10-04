0137ba9648a6fa592fda09a131982bc8af6ac659
Alex Lin <ardt4113c@gmail.com>
2018-10-04 23:11:03 +0800

    fork

--- HW1/hw1_1.c
+++ HW1/hw1_1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define MAX_LINE 80
 char	*arg[MAX_LINE/2+1];      /*command line arguments*/
@@ -12,33 +14,59 @@ int main(void){
 
 	while(should_run){
 
-		char 	InputBuffer[MAX_LINE+1];
+		char 	*InputBuffer;
 		char	*tmp = NULL;
-		int   	arg_len = 0;
+		int   	arg_cnt = 0;
+		int 	ampersand = 0;
 		printf("osh>");
 		fflush(stdout);
 		
 		read(STDIN_FILENO, InputBuffer, MAX_LINE);
-		
-		arg[0] = strtok(InputBuffer," \n");
-		tmp = arg[0];
-		
-		
-		for(int i = 1; tmp != NULL ;i++){
-			tmp = strtok(NULL, " \n");
-			arg[i] = tmp;
-			//arg_len++;
-		}
-		for(int i = 0; i <= 40 ;i++){
-			printf("%d %s\n", i, arg[i]);
-		}
-		printf("\n");
-		
-		int pid;
-		pid = fork();
+		int len = strlen(InputBuffer);
+		InputBuffer = strtok(InputBuffer, "\n");
+		//InputBuffer[len-1] = '\0';
 
-		int err = execvp(arg[0], arg);
-		printf("err:%d\n", err);
+
+		if(InputBuffer[len-2] == '&') ampersand = 1;
+		else ampersand = 0;
+		//printf("%d\n", ampersand);
+
+
+		if(strcmp(InputBuffer, "exit") == 0){
+			should_run = 0;
+			break;
+		}
+
+		arg[0] = strtok(InputBuffer," ");
+		tmp = arg[0];
+		for(int i = 1; tmp != NULL ;i++){
+			tmp = strtok(NULL, " ");
+			arg[i] = tmp;
+			arg_cnt++;
+		}
+		
+
+		pid_t pid = fork();
+
+		if(pid < 0){         //error 
+			fprintf(stderr, "Fork failed!");
+		}
+		else if(pid == 0){  //child process
+			int forkmsg = execvp(arg[0], arg);
+		}
+		else{               //parent process   
+			wait(NULL);
+			printf("Child complete.\n");
+		}
+		
+
+
+
+
+
+
+		//int err = execvp(arg[0], arg);
+		//printf("err:%d\n", err);
 
 		/**
 		* your code!

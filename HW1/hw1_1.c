016472eb024e8480aa3dcf357af58f18e083f6a0
Alex Lin <ardt4113c@gmail.com>
2018-10-05 21:53:28 +0800

    finish with comments

--- HW1/hw1_1.c
+++ HW1/hw1_1.c
@@ -6,95 +6,66 @@
 #include <sys/wait.h>
 
 #define MAX_LINE 80
-char	*arg[MAX_LINE/2+1];      /*command line arguments*/
+char	*arg[MAX_LINE/2+1];           /*command line arguments*/
 
 int main(void){
-
 	int 	should_run = 1;           /*flag to determine when to exit program*/
-
-
 	while(should_run){
 
-		char 	InputBuffer[80];
+		char 	*InputBuffer;
 		char	*tmp = NULL;
-		int   	arg_cnt = 0;
 		int 	ampersand = 0;
 
-		for(int i = 0; i <= MAX_LINE; i++)
+		/*clean up buffer*/
+		for(int i = 0; i < MAX_LINE; i++)
 			InputBuffer[i] = '\0';
-
-
-
+		
 		printf("osh>");
 		fflush(stdout);
 
-
-
-
+		/*read from std input*/
 		read(STDIN_FILENO, InputBuffer, MAX_LINE);
 		int len = strlen(InputBuffer);
-		//InputBuffer = strtok(InputBuffer, "\n");
-		InputBuffer[len-1] = '\0';
+		InputBuffer = strtok(InputBuffer, "\n");   // erase '\n' at end of line
 
-
+		/*check if it's an ampersand command or not*/
 		if(InputBuffer[len-2] == '&') {
 			ampersand = 1;
 			InputBuffer[len-2] = '\0';
 		}
 		else ampersand = 0;
-		//printf("%d\n", ampersand);
 		
-		for(int i = 0; i < len; i++)
-		{
-			/* code */
-			printf("%d %d %c\n", i, InputBuffer[i], InputBuffer[i]);
-		}
-		
-
-		printf("len:%d\n", len);
-		printf("%c\n", InputBuffer[len-2]);
-		printf("%s\n", InputBuffer);
-
+		/*check if it's an "exit" command*/
 		if(strcmp(InputBuffer, "exit") == 0){
 			should_run = 0;
 			break;
 		}
 
+		/*slice the input command*/
 		arg[0] = strtok(InputBuffer," ");
 		tmp = arg[0];
 		for(int i = 1; tmp != NULL ;i++){
 			tmp = strtok(NULL, " ");
 			arg[i] = tmp;
-			//arg_cnt++;
 		}
 		
-
+		/*fork*/
 		pid_t pid = fork();
-
 		if(pid < 0){         //error 
 			fprintf(stderr, "Fork failed!");
 		}
 		else if(pid == 0){  //child process
 			int forkmsg = execvp(arg[0], arg);
 			exit(0);
-
-
 		}
-		else{               //parent process   
+		else{               //parent process
+			/* if it is an ampersand command, 
+			the parent process does not have to wait for current child process. */   
 			if(!ampersand)
 				waitpid(pid, NULL, 0);
-			printf("Child complete.\n");
+			//printf("Child complete.\n");
 		}
 		
-
-		/*
-		for(int i = 0; i <= len; i++)
-			InputBuffer[i] = '\0';
-		*/
-
-		//int err = execvp(arg[0], arg);
-		//printf("err:%d\n", err);
-
 		/**
 		* your code!
 		* After reading user input, the step are:
@@ -103,7 +74,6 @@ int main(void){
 		* (3) if command included &, parent will not invoke wait()
 		*/
 	}
-
 	return 0;
 }
 
